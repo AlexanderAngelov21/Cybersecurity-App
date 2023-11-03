964db2c9fb4a448c4ad8f64d982a558c6ae6ca43
Alexander Angelov <scorpiona_4500@abv.bg>
2023-11-04 01:52:11 +0200

    Implementing check method to search for open ports in a site

--- CybersecurityApp/Program.cs
+++ CybersecurityApp/Program.cs
@@ -26,7 +26,8 @@ namespace CybersecurityApp
             {
                 Console.WriteLine("\nPlease select an option:");
                 Console.WriteLine("1. Ping a website");
-                Console.WriteLine("2. Exit");
+                Console.WriteLine("2. Check open ports on a remote host");
+                Console.WriteLine("3. Exit");
                 var choice = Console.ReadLine();
 
                 switch (choice)
@@ -36,8 +37,12 @@ namespace CybersecurityApp
                         var website = Console.ReadLine();
                         PingWebsite(website);
                         break;
-                 
                     case "2":
+                        Console.Write("\nEnter a remote host IP address to check open ports: ");
+                        var ipAddress = Console.ReadLine();
+                        CheckOpenPorts(ipAddress);
+                        break;
+                    case "3":
                         Console.WriteLine("\nExiting...");
                 
                         return;         
@@ -63,6 +68,38 @@ namespace CybersecurityApp
                 else
                 {
                     Console.WriteLine($"\n{website} is unreachable.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nAn error occurred: {ex.Message}");
+            }
+        }
+        static void CheckOpenPorts(string ipAddress)
+        {
+            try
+            {
+                var portsToCheck = new[] { 80, 443, 22, 3389, 1433 };
+
+                Console.WriteLine($"\nScanning open ports on {ipAddress}");
+
+                foreach (var port in portsToCheck)
+                {
+                    var client = new TcpClient();
+
+                    try
+                    {
+                        client.Connect(ipAddress, port);
+                        Console.WriteLine($"Port {port} is open");
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Port {port} is closed");
+                    }
+                    finally
+                    {
+                        client.Close();
+                    }
                 }
             }
             catch (Exception ex)
